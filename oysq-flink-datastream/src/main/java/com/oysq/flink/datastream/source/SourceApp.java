d5f13e51c5320dc91e8d60f069d7d7d0bc19dbf6
oysq <1585228683@qq.com>
2021-10-19 00:05:36 +0800

    flink核心API

--- oysq-flink-datastream/src/main/java/com/oysq/flink/datastream/source/SourceApp.java
+++ oysq-flink-datastream/src/main/java/com/oysq/flink/datastream/source/SourceApp.java
@@ -1,9 +1,14 @@
 package com.oysq.flink.datastream.source;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.Properties;
 
 public class SourceApp {
 
@@ -13,28 +18,47 @@ public class SourceApp {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         // 测试socket及其并行度
-        test01(env);
+        // test01(env);
+        test02(env);
 
         // 执行
         env.execute("sourceApp");
 
     }
 
+    /**
+     * 测试并行度
+     * @param env
+     */
     public static void test01(StreamExecutionEnvironment env) {
 
-//        env.setParallelism(2);// 全局设置
+        // env.setParallelism(2);// 全局设置
 
         DataStreamSource<String> source = env.socketTextStream(System.getenv("SERVER-HOST"), 9527);// 默认是1
-//        source.setParallelism(1);// 优先级最高，这里只能设置为1
+        // source.setParallelism(1);// 优先级最高，这里只能设置为1
         System.out.println("source 的并行度：" + source.getParallelism());
 
         SingleOutputStreamOperator<String> filter = source.filter(s -> StringUtils.isNotBlank(s));// 默认是cpu核心数，优先级最低
-//        filter.setParallelism(4);// 优先级最高
+        // filter.setParallelism(4);// 优先级最高
         System.out.println("filter 的并行度：" + filter.getParallelism());
 
         // 输出结果
         filter.print();
+    }
 
+    /**
+     * 对接kafka数据源
+     * @param env
+     */
+    public static void test02(StreamExecutionEnvironment env) {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("group.id", "test");
+        DataStream<String> stream = env
+                .addSource(new FlinkKafkaConsumer<>("topic", new SimpleStringSchema(), properties));
+
+        System.out.println(stream.getParallelism());
+        stream.print();
     }
 
 }
