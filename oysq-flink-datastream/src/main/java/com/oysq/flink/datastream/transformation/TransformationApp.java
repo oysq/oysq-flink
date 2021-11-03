1b6d0d0b1cab8384e8f5909817fa5456673424c7
oysq <oysq_super@qq.com>
2021-11-04 00:25:29 +0800

    算子

--- oysq-flink-datastream/src/main/java/com/oysq/flink/datastream/transformation/TransformationApp.java
+++ oysq-flink-datastream/src/main/java/com/oysq/flink/datastream/transformation/TransformationApp.java
@@ -1,11 +1,13 @@
 package com.oysq.flink.datastream.transformation;
 
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.functions.FormattingMapper;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 import java.util.function.Function;
 
@@ -27,7 +29,10 @@ public class TransformationApp {
     /**
      * 算子处理
      *
+     * filter：过滤满足条件的数据
      * map：作用在每一个元素上，进来是多少个，出去就是多少个
+     * flatMap：作用在每个元素上，一进多出，这个多也可能是一个或没有
+     *
      *
      */
     private static void deal(StreamExecutionEnvironment env) {
@@ -39,7 +44,15 @@ public class TransformationApp {
                 String[] strArr = str.split(",");
                 return new Access(strArr[0], strArr[1], Long.valueOf(strArr[2]));
             })
-            .filter(access -> access.getNum() > 1000);
+            .filter(access -> access.getNum() > 1000)
+            .flatMap(new FlatMapFunction<Access, Access>() {
+                @Override
+                public void flatMap(Access access, Collector<Access> collector) {
+                    collector.collect(access);
+                    access.setUrl(access.getUrl()+"/index");
+                    collector.collect(access);
+                }
+            });
 
         res.print();
 
