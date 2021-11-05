0f9633ebca59a39fef4e5cbc64e0125fe75e1865
oysq <1585228683@qq.com>
2021-11-05 21:10:48 +0800

    sink to kafka

--- oysq-flink-datastream/src/main/java/com/oysq/flink/datastream/sink/SinkApp.java
+++ oysq-flink-datastream/src/main/java/com/oysq/flink/datastream/sink/SinkApp.java
@@ -1,7 +1,12 @@
 package com.oysq.flink.datastream.sink;
 
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.streaming.connectors.kafka.table.KafkaDynamicSink;
+import org.apache.kafka.clients.producer.KafkaProducer;
 
 public class SinkApp {
 
@@ -14,8 +19,12 @@ public class SinkApp {
         DataStreamSource<String> source = env.socketTextStream("localhost", 9527);
         System.out.println("source 的并行度：" + source.getParallelism());
 
+        // sink -> stdout
         source.print("msg");
-        source.printToErr("msg");
+//        source.printToErr("msg");
+
+        // sink -> kafka
+        source.addSink(new FlinkKafkaProducer<>(System.getenv("KAFKA-SERVER"), "test01", new SimpleStringSchema()));
 
         // 执行
         env.execute("SinkApp");
