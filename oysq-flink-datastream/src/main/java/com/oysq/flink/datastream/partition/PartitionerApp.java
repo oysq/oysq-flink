922ff1556c59a954dfa615a7d6cb72f8775d98d5
oysq <1585228683@qq.com>
2021-11-11 23:14:55 +0800

    Partitioner 分区

--- oysq-flink-datastream/src/main/java/com/oysq/flink/datastream/partition/PartitionerApp.java
+++ oysq-flink-datastream/src/main/java/com/oysq/flink/datastream/partition/PartitionerApp.java
@@ -11,8 +11,10 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class PartitionerApp {
 
     /**
-     * 分区策略决定的是一条数据要分给自己上游的哪个分区，所以需要注意的是上游的分区数设置不能小于分区器需要的数量
-     * 注意：partitionCustom() 不会改变上游的分区数量
+     * 分区策略决定的是一条数据要分给自己上游算子的哪个分区，所以需要注意的是上游算子的分区数设置不能小于分区器需要的数量
+     * 注意：
+     * 1.partitionCustom() 只是指定流规则，不会改变上游的分区数量
+     * 2.只会影响跟在后面的第一个算子，再往后的算子与此无关
      */
     public static void main(String[] args) throws Exception {
 
@@ -22,33 +24,24 @@ public class PartitionerApp {
 
         // source
         DataStreamSource<User> source = env.addSource(new MysqlSource()).setParallelism(2);
-        System.out.println("source: " + source.getParallelism());
+        System.out.println("source before: " + source.getParallelism());
 
-        // transformation
-        DataStream<User> userDataStream = source.partitionCustom(
+        // 设置 自定义分区器
+        DataStream<User> customSource = source.partitionCustom(
                 new OysqPartitioner(),
                 (KeySelector<User, Integer>) User::getAge
         );
-        System.out.println("dataStream: " + userDataStream.getParallelism());
-        DataStream<User> dataStream = userDataStream.map(user -> {
-            System.out.println("map1 => " + Thread.currentThread().getId() + "  " + user.getAge());
+        System.out.println("source after: " + source.getParallelism());
+
+        // transformation
+        DataStream<User> dataStream = customSource.map(user -> {
+            System.out.println("map => " + Thread.currentThread().getId() + "  " + user.getAge());
             return user;
-        });
+        }).setParallelism(4);
         System.out.println("dataStream: " + dataStream.getParallelism());
 
         // sink
-        dataStream.print();
-
-//
-//        userDataStream.map(user -> {
-//                    System.out.println("map1 => " + Thread.currentThread().getId() + "  " + user.getAge());
-//                    return user;
-//                }).setParallelism(4)
-//                .map(user -> {
-//                    System.out.println("map2 => " + Thread.currentThread().getId() + "  " + user.getAge());
-//                    return user;
-//                }).setParallelism(4)
-//                .print(Thread.currentThread().getId()+"->").setParallelism(5);
+        dataStream.print().setParallelism(2);
 
         // 执行
         env.execute("PartitionerApp");
