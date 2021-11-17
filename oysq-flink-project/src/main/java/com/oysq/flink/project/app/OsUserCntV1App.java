64ead44b2f6120b8d431f61c40f43d0ac82bca29
oysq <1585228683@qq.com>
2021-11-17 23:55:34 +0800

    整理代码

--- oysq-flink-project/src/main/java/com/oysq/flink/project/app/OsUserCntV1App.java
+++ oysq-flink-project/src/main/java/com/oysq/flink/project/app/OsUserCntV1App.java
@@ -23,19 +23,20 @@ public class OsUserCntV1App {
         DataStreamSource<String> source = env.readTextFile("data/access.json");
 
         source
-            .map(item -> JSON.parseObject(item, Access.class))
-            .filter(access -> "startup".equals(access.getEvent()))
-            .map(access -> Tuple3.of(access.getOs(), access.getNu(), 1))
-            .returns(TypeInformation.of(new TypeHint<Tuple3<String, Integer, Integer>>() {}))
-            .keyBy(new KeySelector<Tuple3<String, Integer, Integer>, Tuple2<String, Integer>>() {
-                @Override
-                public Tuple2<String, Integer> getKey(Tuple3<String, Integer, Integer> value) throws Exception {
-                    return Tuple2.of(value.f0, value.f1);
-                }
-            })
-            .sum(2)
-            .print()
-            .setParallelism(1);
+                .map(item -> JSON.parseObject(item, Access.class))
+                .filter(access -> "startup".equals(access.getEvent()))
+                .map(access -> Tuple3.of(access.getOs(), access.getNu(), 1))
+                .returns(TypeInformation.of(new TypeHint<Tuple3<String, Integer, Integer>>() {
+                }))
+                .keyBy(new KeySelector<Tuple3<String, Integer, Integer>, Tuple2<String, Integer>>() {
+                    @Override
+                    public Tuple2<String, Integer> getKey(Tuple3<String, Integer, Integer> value) throws Exception {
+                        return Tuple2.of(value.f0, value.f1);
+                    }
+                })
+                .sum(2)
+                .print()
+                .setParallelism(1);
 
         // 执行
         env.execute("OsUserCntV1");
