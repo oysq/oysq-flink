1d652de163b467bdcab893e52b487bf595f876c5
oysq <1585228683@qq.com>
2021-12-05 16:52:22 +0800

    Kafka 转 redis

--- oysq-flink-project/src/main/java/com/oysq/flink/project/app/KafkaFlinkAppV2.java
+++ oysq-flink-project/src/main/java/com/oysq/flink/project/app/KafkaFlinkAppV2.java
@@ -6,12 +6,14 @@ import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
 import org.apache.flink.util.Collector;
 
 import java.util.Arrays;
 
-public class KafkaFlinkAppV1 {
+public class KafkaFlinkAppV2 {
 
     public static void main(String[] args) throws Exception {
 
@@ -21,8 +23,9 @@ public class KafkaFlinkAppV1 {
         // 配置环境
         ConfigUtil.setEnvironment(env, args);
 
-        // 添加数据源
-        env.addSource(ConfigUtil.createKafkaConsumer(env, args, SimpleStringSchema.class))
+        // source + operator
+        SingleOutputStreamOperator<Tuple2<String, Integer>> operator =
+                env.addSource(ConfigUtil.createKafkaConsumer(env, args, SimpleStringSchema.class))
                 .flatMap(new FlatMapFunction<String, String>() {
                     @Override
                     public void flatMap(String value, Collector<String> out) throws Exception {
@@ -39,24 +42,16 @@ public class KafkaFlinkAppV1 {
                     }
                 })
                 .keyBy(x -> x.f0)
-                .sum(1)
-                .print("词频统计：");
+                .sum(1);
 
-        // 模拟失败
-        env.socketTextStream("localhost", 9527)
-                .map(new MapFunction<String, String>() {
-                    @Override
-                    public String map(String value) throws Exception {
-                        if (StringUtils.isNotBlank(value) && "pk".equals(value)) {
-                            throw new RuntimeException("PK了呀~~~");
-                        }
-                        return value;
-                    }
-                })
-                .print("Socket Data：");
+        // 打印
+        operator.print("词频统计：");
+
+        // 写入 redis
+        operator.addSink(ConfigUtil.createRedisSink(args));
 
         // 执行
-        env.execute("KafkaFlinkAppV1");
+        env.execute("KafkaFlinkAppV2");
 
     }
 }
--- oysq-flink-project/src/main/java/com/oysq/flink/project/utils/ConfigUtil.java
+++ oysq-flink-project/src/main/java/com/oysq/flink/project/utils/ConfigUtil.java
@@ -2,13 +2,18 @@ package com.oysq.flink.project.utils;
 
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.time.Time;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -19,16 +24,26 @@ import java.util.stream.Collectors;
 
 public class ConfigUtil {
 
-    public static <T> FlinkKafkaConsumer<T> createKafkaConsumer(StreamExecutionEnvironment env, String[] args, Class<? extends DeserializationSchema<T>> deserialization) throws Exception {
-
+    /**
+     * 配置 environment
+     */
+    public static void setEnvironment(StreamExecutionEnvironment env, String[] args) throws IOException {
         // 获取配置
         ParameterTool tool = ParameterTool.fromPropertiesFile(args[0]);
-
         // checkpoint 相关配置
         env.enableCheckpointing(tool.getInt("checkpoint.interval", 30000));// 30秒一次
         env.setStateBackend(new FsStateBackend(tool.getRequired("checkpoint.statebackend.dataurl")));
         env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
         env.setRestartStrategy(RestartStrategies.fixedDelayRestart(5, Time.of(1, TimeUnit.SECONDS)));
+    }
+
+    /**
+     * 获取 Kafka Source
+     */
+    public static <T> FlinkKafkaConsumer<T> createKafkaConsumer(StreamExecutionEnvironment env, String[] args, Class<? extends DeserializationSchema<T>> deserialization) throws Exception {
+
+        // 获取配置
+        ParameterTool tool = ParameterTool.fromPropertiesFile(args[0]);
 
         // kafka相关参数配置
         Properties properties = new Properties();
@@ -45,6 +60,26 @@ public class ConfigUtil {
         return new FlinkKafkaConsumer<>(topicList, deserialization.newInstance(), properties);
     }
 
+    /**
+     * 获取 Redis Sink
+     */
+    public static RedisSink createRedisSink(String[] args) throws IOException {
+
+        // 获取配置
+        ParameterTool tool = ParameterTool.fromPropertiesFile(args[0]);
+
+        // 配置服务器信息
+        FlinkJedisPoolConfig config =
+                new FlinkJedisPoolConfig
+                        .Builder()
+                        .setHost(tool.get("redis.host"))
+                        .setPort(Integer.parseInt(tool.getRequired("redis.port")))
+                        .setPassword(tool.getRequired("redis.password"))
+                        .build();
+
+        return new RedisSink<>(config, new SinkRedisMapper());
+    }
+
     public static void main(String[] args) throws IOException {
 
         ParameterTool tool = ParameterTool.fromPropertiesFile(args[0]);
@@ -57,4 +92,23 @@ public class ConfigUtil {
 
     }
 
+}
+
+class SinkRedisMapper implements RedisMapper<Tuple2<String, Integer>> {
+
+    @Override
+    public RedisCommandDescription getCommandDescription() {
+        // 设置表结构、表名
+        return new RedisCommandDescription(RedisCommand.HSET, "wc");
+    }
+
+    @Override
+    public String getKeyFromData(Tuple2<String, Integer> value) {
+        return value.f0;
+    }
+
+    @Override
+    public String getValueFromData(Tuple2<String, Integer> value) {
+        return value.f1.toString();
+    }
 }
